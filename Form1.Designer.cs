77f302cac05c17255868554b43fdce55d9370a5c
Gustavo Daniel Britez Alvarenga <115171270+GustavoBritez@users.noreply.github.com>
2023-09-09 15:02:44 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ejercicio7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_resultado = new System.Windows.Forms.TextBox();
+            this.bt_iniciar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txt_resultado
+            // 
+            this.txt_resultado.Location = new System.Drawing.Point(164, 59);
+            this.txt_resultado.Name = "txt_resultado";
+            this.txt_resultado.Size = new System.Drawing.Size(442, 20);
+            this.txt_resultado.TabIndex = 0;
+            // 
+            // bt_iniciar
+            // 
+            this.bt_iniciar.Location = new System.Drawing.Point(310, 180);
+            this.bt_iniciar.Name = "bt_iniciar";
+            this.bt_iniciar.Size = new System.Drawing.Size(75, 23);
+            this.bt_iniciar.TabIndex = 1;
+            this.bt_iniciar.Text = "Comenzar";
+            this.bt_iniciar.UseVisualStyleBackColor = true;
+            this.bt_iniciar.Click += new System.EventHandler(this.bt_iniciar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.bt_iniciar);
+            this.Controls.Add(this.txt_resultado);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_resultado;
+        private System.Windows.Forms.Button bt_iniciar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio7
+{
+    public partial class Form1 : Form
+    {
+        List<int> num = new List<int> { 1, 2, 3 };
+        List<char> let = new List<char> { 'a', 'b', 'c' };
+        List<Tuple<int, char>> listaCombinada= new List<Tuple<int, char>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<Tuple<int, char>> Aparear(List<int> num, List<char> let)
+        {
+
+            if ( 0 == num.Count)
+            { 
+                   return new List<Tuple<int, char>>();
+            }
+            //Combina elementos de ambas listas
+            var tupla = Tuple.Create(num[0], let[0]);
+
+            // Llamada recursiva con las listas reducidas
+            var resultadoRecursivo = Aparear(num.GetRange(1, num.Count - 1), let.GetRange(1, let.Count - 1));
+
+            resultadoRecursivo.Insert(0, tupla);
+
+            return resultadoRecursivo;
+
+
+        }
+
+        private void bt_iniciar_Click(object sender, EventArgs e)
+        {
+            
+
+            listaCombinada = Aparear(num, let);
+
+            foreach (var tupla in listaCombinada)
+            {
+                txt_resultado.Text+=("(" + tupla.Item1 + ", '" + tupla.Item2 + "')")+ "   ";
+            }
+
+
+        }
+    }
+}
